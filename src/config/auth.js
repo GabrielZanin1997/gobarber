8286ce04995ac4db052e8f4009fcccb1d2429cfb
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2021-04-07 20:42:47 -0300

    building routes

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd499b08dec8cf304c501e2af5450e796',
+  expiresIn: '7d'
+}
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,29 @@
 import { Router } from 'express';
 
-const routes = new Router();
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-routes.get('/', (req, res) => {
-  return res.json({message: 'Hello world'});
-})
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
