04f66034791c7a1b9886e6bba6695ed9201917ec
GabrielZanin1997 <gabriel.zaninde@gmail.com>
2021-04-11 18:00:41 -0300

    sending confirmation email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '30e38153929894',
+    pass: '355a42e497b859',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
